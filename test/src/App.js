825da8d836632feba5ae58242a979fda84992c34
rakon1 <ykrayyem@gmail.com>
2019-01-18 09:29:40 +0200

    ping

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+
 
 class App extends Component {
+  state = {
+    isConnected:false
+  }
+  socket = null
+
+  componentDidMount(){
+
+    this.socket = io('http://codicoda.com:8000');
+
+    this.socket.on('connect', () => {
+      this.setState({isConnected:true})
+    })
+    this.socket.on('pong!',()=>{
+      console.log('the server answered!')
+    })
+    
+
+    this.socket.on('disconnect', () => {
+      this.setState({isConnected:false})
+    })
+
+    /** this will be useful way, way later **/
+    this.socket.on('room', old_messages => console.log(old_messages))
+
+
+  }
+
+  componentWillUnmount(){
+    this.socket.close()
+    this.socket = null
+  }
+
   render() {
     return (
-    <div>ss</div>
-    );
+      <div>status: {this.state.isConnected ? 'connected' : 'disconnected'}
+              <button onClick={()=>this.socket.emit('ping!')}>ping</button>
+
+      
+      
+      </div>
+      
+      );
   }
 }
 
